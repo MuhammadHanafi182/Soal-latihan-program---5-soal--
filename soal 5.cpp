11c3b0db4e1467c51a1d0c546b823c5888ceed0a
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2017-11-07 00:32:31 +0700

    Penentuan tinggi badan yang termasuk kategori Underweight,Overweight dan normal

--- soal 5.cpp
+++ soal 5.cpp
@@ -10,4 +10,11 @@ int main(){
 	cin >>tinggibadan;
     cout << "Silahkan masukkan berat badan (kg) : " ;
     cin >> beratbadan;
+    
+    if (beratbadan<tinggibadan/2.5)
+    cout << "  Maaf untuk saat ini anda kekurangan berat badan (UnderWeight) \n";
+    else if (tinggibadan/2.3 < beratbadan)
+    cout << "  Maaf untuk saat ini anda kelebihan berat badan (Overweight) \n";
+    else if (tinggibadan/2.5 <= beratbadan <= tinggibadan/2.3)
+	cout << "  Selamat berat badan dan tinggi badan anda proporsional (normal) \n";
 }
