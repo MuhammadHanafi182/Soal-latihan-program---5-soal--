152aaa6afbd21b4bbb9ee3addd7c6f2f89521958
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2017-11-07 00:07:08 +0700

    Penginputaan bilangan yang ingin dicari tau positif atau negatif atau nol nya

--- Soal 4.cpp
+++ Soal 4.cpp
@@ -3,4 +3,11 @@ using namespace std;
 
 int main(){
 	int bil;
+	
+	cout<<"Program menentukan jenis biangan Negatif,Positif,nol \n" ;
+	cout<<" \n ";
+	cout<<" Input Bilangan Yang Anda Inginkan : ";
+	cin>>bil;
+	cout<<" \n ";
+	
 }
