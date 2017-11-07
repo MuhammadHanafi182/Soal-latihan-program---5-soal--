69217786cf88d353a96750891b89036c2a2a54cc
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2017-11-07 22:45:55 +0700

    Memasukan Jam kerja karyawan dalam seminggu dan gaji perjam karyawan

--- Soal 3.cpp
+++ Soal 3.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main(){
 	float jaker, gaper, gatot,totaljamlem, gapok, jamlembur,bonlem,gaber;
+	
+	cout<<"   Program Perhitungan Gaji Karyawan \n";
+	cout<<""<<endl;
+	cout<<"Masukan Jam kerja (seminggu)    : "; 
+	cin>>jaker;
+	cout<<"Gaji Perjam Karyawan   : "; 
+	cin>>gaper;
 
 }
